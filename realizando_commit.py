c86ef6d9818e40706dc98a6418afab318d1855ad
jeanheberth <jeanheberth19@gmail.com>
2025-05-03 20:30:23 -0300

    subindo alterações

---
+++ realizando_commit.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+
+REPO_PATH = "/Users/jeanheberth/Documents/GitClone/FrontEnd/BorrachariaPerola" # Defina o caminho do repositório aqui
+
+import subprocess
+
+def run_command(command):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True, cwd=REPO_PATH)
+    if result.returncode != 0:
+        print(f"Erro ao executar: {command}\n{result.stderr}")
+    else:
+        print(result.stdout)
+
+def git_commit():
+    commit_message = input("Digite a mensagem do commit: ")
+
+    print("\nAdicionando arquivos ao commit...")
+    run_command("git add .")
+
+    print(f"\nRealizando commit com a mensagem: {commit_message}...")
+    run_command(f"git commit -m \"{commit_message}\"")
+
+    print("\nCommit e push realizados com sucesso!")
+
+if __name__ == "__main__":
+    git_commit()
